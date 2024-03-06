807ab8d2629f1d8b77867969a5ee131f49e3ad95
song <55rsearchco@gmail.com>
2024-03-06 20:57:24 +0900

    Commad-line flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// 중요한 점은 flag.Parse() 함수를 사용하여 명령줄 플래그를 구문 분석한다는 것입니다.
+	// 이는 명령줄 플래그 값을 읽고 이를 addr에 할당합니다.
+	// 변수. addr 변수를 사용하기 *전에* 이것을 호출해야 합니다
+	// 그렇지 않으면 항상 기본값 ":4000"이 포함됩니다. 오류가 있는 경우
+	// 구문 분석 중에 발생하면 애플리케이션이 종료됩니다.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -18,7 +28,10 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// flag.String() 함수에서 반환된 값은 플래그에 대한 포인터입니다.
+	// 값, 값 자체가 아닙니다. 따라서 포인터를 역참조해야 합니다. (즉, 앞에 * 기호를 붙입니다).
+	// 로그 메시지에 주소를 삽입하기 위해 log.Printf() 함수를 사용하고 있다는 점에 유의하세요.
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }

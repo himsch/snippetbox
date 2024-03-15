a7ab3c123f258efd77b29668b3c4f685f84bb046
song <55rsearchco@gmail.com>
2024-03-15 15:16:50 +0900

    Caching templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,46 @@
 package main
 
-import "snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+	"snippetbox/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// 전체 파일 경로에서 파일 이름(예: 'home.tmpl')을 추출합니다.
+		name := filepath.Base(page)
+
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }

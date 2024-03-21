e97e52d4c5b5fc235535f97d6950f43b3183bb14
song <55rsearchco@gmail.com>
2024-03-21 17:35:22 +0900

    Cookies and redirections

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -25,6 +26,24 @@ type testServer struct {
 // 사용자 정의 testServer 유형의 새 인스턴스를 초기화하고 반환하는 newTestServer 도우미를 만듭니다.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	// 새 쿠키 jar를 초기화합니다.
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 테스트 서버 클라이언트에 쿠키 jar를 추가합니다.
+	// 이제 이 클라이언트를 사용할 때 모든 응답 쿠키가 저장되고 후속 요청과 함께 전송됩니다.
+	ts.Client().Jar = jar
+
+	// 사용자 정의 CheckRedirect 기능을 설정하여 테스트 서버 클라이언트에 대한 리디렉션 따르기를 비활성화합니다.
+	// 이 함수는 클라이언트가 3xx 응답을 수신할 때마다 호출되며
+	// 항상 http.ErrUseLastResponse 오류를 반환하여 클라이언트가 수신된 응답을 즉시 반환하도록 합니다.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
